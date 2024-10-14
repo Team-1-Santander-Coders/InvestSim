a0287fbc9c61c92c2d0e87f6889bcef4446a20cd
Matheus Lima Moreira <102155883+mathlimam@users.noreply.github.com>
2024-10-14 10:44:57 -0300

    insert project structure

---
+++ src/main/java/com/team1/investsim/InvestsimApplication.java
@@ -0,0 +1,13 @@
+package com.team1.investsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestsimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestsimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team1/investsim/InvestsimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.investsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestsimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
