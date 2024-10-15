6eb9a2ab2cbf9d379a64d686fcaa780093c2d7f8
Alecsandro Schopf Auer Junior <alexschopf05@gmail.com>
2024-10-15 20:55:20 -0300

    feat: add getCurrentValue method to AssetHoldingDTO

--- src/main/java/com/team1/investsim/dtos/AssetHoldingDTO.java
+++ src/main/java/com/team1/investsim/dtos/AssetHoldingDTO.java
@@ -1,12 +1,9 @@
 package com.team1.investsim.dtos;
 
-import com.team1.investsim.Utils.DateUtil;
-import com.team1.investsim.exceptions.IllegalDateException;
-
-import java.time.LocalDateTime;
+import java.math.BigDecimal;
 
 public record AssetHoldingDTO(long id, AssetDTO asset, double quantity, TranscationDTO transcation) {
-    public double getValue(LocalDateTime date) throws IllegalDateException {
-        return 0;
+    public BigDecimal getCurrentValue(){
+        return BigDecimal.valueOf(asset.currentPrice() * this.quantity);
     }
 }
