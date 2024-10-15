81849ccc09611fcc36f7e9e1a026bdd304dcfeb0
Alecsandro Schopf Auer Junior <alexschopf05@gmail.com>
2024-10-15 19:01:51 -0300

    feat: add HistoricalDataNotFound exception

---
+++ src/main/java/com/team1/investsim/exceptions/HistoricalDataNotFoundException.java
@@ -0,0 +1,7 @@
+package com.team1.investsim.exceptions;
+
+public class HistoricalDataNotFoundException extends Exception {
+    public HistoricalDataNotFoundException(String message) {
+        super(message);
+    }
+}
