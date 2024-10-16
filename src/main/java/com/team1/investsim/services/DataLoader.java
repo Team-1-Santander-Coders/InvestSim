e193ce3ff10bd8b89ff58b134444ddd33a8733c5
Matheus Lima Moreira <102155883+mathlimam@users.noreply.github.com>
2024-10-16 12:27:54 -0300

    refactor: change loadData method to receive a single entity instead of a list

--- src/main/java/com/team1/investsim/services/DataLoader.java
+++ src/main/java/com/team1/investsim/services/DataLoader.java
@@ -3,8 +3,6 @@ package com.team1.investsim.services;
 import com.team1.investsim.entities.AssetEntity;
 import com.team1.investsim.entities.HistoricalDataEntity;
 import com.team1.investsim.exceptions.IllegalDateException;
-import com.team1.investsim.repositories.asset.AssetRepository;
-import com.team1.investsim.repositories.asset.AssetRepositoryImpl;
 import com.team1.investsim.utils.CSVProcessor;
 import com.team1.investsim.utils.DateUtil;
 
@@ -14,42 +12,38 @@ import java.util.List;
 import java.util.Optional;
 
 public class DataLoader {
-    AssetRepository assetRepository;
 
-    public void loadData(String filepath) {
-        assetRepository = new AssetRepositoryImpl();
-        List<AssetEntity> assetEntityList = assetRepository.findAll();
-
+    public static void loadData(AssetEntity assetEntity, String filepath) {
         Optional<List<String[]>> assetsDataOptional = CSVProcessor.processCSV(filepath);
 
         if (assetsDataOptional.isPresent()) {
             List<String[]> assetsData = assetsDataOptional.get();
+            List<HistoricalDataEntity> historicalDataEntityList = new ArrayList<>();
+            try {
 
-            assetEntityList.stream().forEach(asset -> {
-                for (AssetEntity assetEntity : assetEntityList) {
-                    List<HistoricalDataEntity> historicalDataEntityList = new ArrayList<>();
-                    for (String[] assetData : assetsData) {
-                        if (assetData[7].equals(asset.getTicker())) {
-
-                            try {
-                                HistoricalDataEntity historicalDataEntity = new HistoricalDataEntity();
-                                historicalDataEntity.setAsset(assetEntity);
-                                historicalDataEntity.setDate(DateUtil.stringToDate(assetData[0], "yyyy-MM-dd"));
-                                historicalDataEntity.setOpenPrice(BigDecimal.valueOf(Double.valueOf(assetData[1])));
-                                historicalDataEntity.setHighPrice(BigDecimal.valueOf(Double.valueOf(assetData[2])));
-                                historicalDataEntity.setLowPrice(BigDecimal.valueOf(Double.valueOf(assetData[3])));
-                                historicalDataEntity.setClosePrice(BigDecimal.valueOf(Double.valueOf(assetData[4])));
-                                historicalDataEntity.setVolume(Double.valueOf(assetData[6]).longValue());
-                                historicalDataEntityList.add(historicalDataEntity);
-
-                            } catch (IllegalDateException e) {
-                                e.printStackTrace();
-                            }
-                        }
+                for (String[] assetData : assetsData) {
+                    if(assetEntity.getTicker().equals(assetData[7])) {
+                        HistoricalDataEntity historicalDataEntity = new HistoricalDataEntity();
+                        historicalDataEntity.setAll(assetEntity, assetData[0], assetData[1], assetData[2], assetData[3], assetData[4], assetData[5]);
+                        historicalDataEntityList.add(historicalDataEntity);
+                        
+                        assetEntity.setHistoricalData(historicalDataEntityList);
                     }
-                    assetEntity.setHistoricalData(historicalDataEntityList);
                 }
-            });
+
+            } catch (IllegalDateException e) {
+                e.printStackTrace();
+            }
+
         }
+    }
+
+    public static void main(String[] args) {
+        AssetEntity assetEntity = new AssetEntity();
+        assetEntity.setTicker("MMM");
+
+        loadData(assetEntity, "sp500_data.csv");
+
+        System.out.println("ticker: " + assetEntity.getTicker() + " - historicalDataLength: "+assetEntity.getHistoricalData().size());
     }
 }
