ad25da14239c68a2fbfb9d1cdb87c0fdf0481145
Maria Matos <mariaeduarda.matos@gmail.com>
2024-10-16 20:17:14 -0300

    Adicionando repositório AssetHolding

---
+++ src/main/java/com/team1/investsim/repositories/AssetHoldingRepository.java
@@ -0,0 +1,7 @@
+package com.team1.investsim.repositories;
+
+import com.team1.investsim.entities.AssetHoldingEntity;
+
+public interface AssetHoldingRepository extends Repository<AssetHoldingEntity> {
+
+}
